aa04b5501f532059a2fd2569ee10145bbc15ecf7
Nicla <faccioli.nicla@gmail.com>
2021-01-08 22:35:15 +0100

    Aggiunta funzioni per prodotti e carrello

--- dbConnection.php
+++ dbConnection.php
@@ -25,6 +25,7 @@ class DBAccess {
 		mysqli_close($this->connection);
 	}
 	
+	// per popolare la pagina carrello
 	public function getListaProdotti($email) {
 		$querySelect = "SELECT * FROM carrello WHERE email_user='$email';";	
 		$queryResult = mysqli_query($this->connection, $querySelect);
@@ -47,9 +48,9 @@ class DBAccess {
 		}
 	}
 	
+	// inserisce un prodotto nel carrello
 	public function inserisciProdotto($email_user, $nome_item, $grandezza) {
 		$queryInserimento= "INSERT INTO carrello(email_user, nome_item, grandezza) VALUES (\"$email_user\", \"$nome_item\", \"$grandezza\")";
-		
 		$queryResult = mysqli_query($this->connection, $queryInserimento);
 		
 		if(mysqli_affected_rows($this->connection) > 0) {
@@ -59,6 +60,58 @@ class DBAccess {
 			return false;
 		}
 	}
+
+	// svuota il carrello quando viene premuto il pulsante per effettuare l'ordine
+	public function svuotaCarrello($email) {
+		$querySvuotamento= "DELETE FROM carrello WHERE email_user='$email';";
+		$queryResult = mysqli_query($this->connection, $querySvuotamento);
+		
+		if(mysqli_affected_rows($this->connection) > 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	// permette all'utente di eliminare un singolo prodotto dal proprio carrello
+	public function eliminaProdotto($email_user, $nome_item) {	//i prodotti nel carrello hanno come chiave primaria questi due campi
+		$queryRimozione= "DELETE FROM carrello WHERE email_user='$email_user' AND $nome_item='nome_item';";
+		$queryResult = mysqli_query($this->connection, $queryRimozione);
+		
+		if(mysqli_affected_rows($this->connection) > 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	//funzione per popolare la pagina prodotti
+	public function getListaItem() {
+		$querySelect = "SELECT * FROM item";	
+		$queryResult = mysqli_query($this->connection, $querySelect);
+		
+		if(mysqli_num_rows($queryResult) == 0) {
+			return null;
+		}
+		else {
+			$listaItem = array();
+			while($riga = mysqli_fetch_assoc($queryResult)) {
+				$item = array(
+					"nome" => $riga['nome'],
+					"descrizione" => $riga['descrizione'],
+					"foto" => $riga['foto'],
+					"nome_category" =>  $riga['nome_category']
+				);
+				
+				array_push($listaItem, $item);
+			}
+			
+			return $listaItem;
+		}
+	}
+
 }
 
 ?>
