6e76a390bd7a9f789e585019a34e68a74422829c
Nicla <faccioli.nicla@gmail.com>
2021-01-22 22:22:51 +0100

    unificazione javascript gallery

--- javascript.js
+++ javascript.js
@@ -8,3 +8,20 @@ function menu() {
 	}
 }
 
+//apre ingrandimento immagine
+function openModal(id){
+	document.getElementById("myModal").style.display="block";
+	document.getElementById(id).style.display="block";
+	document.getElementById("close").focus();
+}
+				
+//chiude ingrandimento immagine
+function closeModal() {
+	var slides=document.getElementsByClassName("slide");
+	document.getElementById("myModal").style.display = "none";
+	for(var i=0; i < slides.length; i++) {
+		if(slides[i].style.display === "block") {
+			slides[i].style.display = "none";
+		}
+	}
+}
